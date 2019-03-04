1af46ace4b8048f6e616a35898daa164eaf96d95
Emma Bell <emmabell42@gmail.com>
2019-03-04 09:32:13 -0500

    Update get_otp.sh

--- get_otp.sh
+++ get_otp.sh
@@ -1,3 +1,5 @@
+# Change the path at the end of line 4 to wherever you've stored your table of OTPs
+
 #!/bin/bash
 awk -v INDEX="$1"  '{if ($1 == INDEX) {print "OTP " INDEX " = "  $2}}' /Users/emmabell/'OneDrive - UHN'/Resources/otp_table.txt
 
